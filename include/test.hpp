fc6c52ddd97bcc7a27a6e97e95bbaca1bbe83116
XPXPv2 <natedparker07@gmail.com>
2020-01-19 13:56:11 -0500

    more test statment work

--- include/test.hpp
+++ include/test.hpp
@@ -1,6 +1,9 @@
 #define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
 #include "RSG.hpp"
+
+#include <algorithm>
 
 using namespace std;
 
@@ -24,5 +27,24 @@ TEST_CASE("RSG gen"){
     list<char> Cset = {'A','B','C'};
     REQUIRE(generator.setCharSet(Cset) == 0);
     REQUIRE(generator.returnSet() == "ABC");
+  }
+  SECTION("Setting and Retreving Strings"){
+    REQUIRE(generator.addString("abc") == 0);
+    REQUIRE(generator.returnList().front() == "abc");
+  }
+  SECTION("Testing Removal of Strings and Sets"){
+    REQUIRE(generator.clearMemory(true,true,true) == 0);
+    REQUIRE(generator.returnListLen() == 0);
+    REQUIRE(generator.returnSet().size() == 0);
+  }
+  SECTION("Testing String Gen"){
+    REQUIRE(generator.setCharSet("abc") == 0);
+    REQUIRE(generator.genString(5).size() == 5);
+  }
+  SECTION("Testing String Generation"){
+    REQUIRE(generator.genStrings(10) == 0);
+    list<string> strings = generator.returnList();
+    strings.sort();
+    REQUIRE(unique(strings.begin(),strings.end()) == strings.end());
   }
 }
