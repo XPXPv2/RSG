c46a5795f7bdf4b9f52fe1c69be3f2a1f25b2cde
XPXPv2 <natedparker07@gmail.com>
2020-01-23 10:50:43 -0500

    added acual use

--- Makefile
+++ Makefile
@@ -14,6 +14,15 @@ bin/main.o: src/main.cpp include/RSG.hpp include/test.hpp
 bin/RSG.o: src/RSG.cpp include/RSG.hpp
 	g++ -g -c -o bin/RSG.o src/RSG.cpp -I include -I libarys -std=c++17 -pthread
 
+
+relase.bin: clean
+	g++ -g -c -o bin/main.o src/main.cpp -I include -I libarys -std=c++17 -pthread
+	g++ -g -c -o bin/RSG.o src/RSG.cpp -I include -I libarys -std=c++17 -pthread
+	g++ -g -o bin/relase.bin bin/main.o bin/RSG.o -std=c++17 -pthread
+	rm bin/main.o
+	rm bin/RSG.o
+
+
 .PHONY: clean
 clean:
 	rm bin/debug.bin
--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,34 @@ using namespace std;
 
 void printList(list<string> vlist, string sep);
 
+// input args are NUMBEROS NUMBEROT LENGTHOS SET
+
 int main(int argc, char const *argv[]) {
   #if TEST == 1
   return RSGtest(argc,argv);
   #endif
+  if(argc < 4){
+    cout << "You did not supply engouh arguments" << endl;
+    return -1;
+  }
+  int NoS , NoT, LoS;
+  string set;
+  NoS = strtol(argv[1],nullptr,0);
+  NoT = strtol(argv[2],nullptr,0);
+  LoS = strtol(argv[3],nullptr,0);
+  set = argv[4];
+  stringGen gen(LoS);
+  gen.setCharSet(set);
+  gen.stringThreadHandler(NoT,NoS);
+  gen.clearMemory(true,false,false);
+  //printList(gen.returnList()," ,");
+  return 0;
+}
+
+void printList(list<string> vlist, string sep){
+  list<string> :: iterator itr;
+  for ( itr = vlist.begin(); itr != vlist.end(); itr++){
+    cout << (*itr) << sep;
+  }
+  cout << endl;
 }
