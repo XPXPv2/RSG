4e3e65ba87a670afd73f1b227a83e7fbaeae7eea
XPXPv2 <49757854+XPXPv2@users.noreply.github.com>
2020-06-16 14:47:21 -0400

    cleaned up ncurces main file and inproved the cmd user interface

--- src/ncurces-interface/main.cpp
+++ src/ncurces-interface/main.cpp
@@ -1,50 +1,9 @@
-
-
-#ifdef NDEBUG
-#define TEST 0
 #include "gui.hpp"
-#else
-#define TEST 1
-#include "test.hpp"
-#endif
-
-
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-void printList(list<string> vlist, string sep);
-void writeList(list<string> vlist,string name);
-
-// input args are NUMBEROS NUMBEROT LENGTHOS SET SAVEFILE
 
 int main(int argc, char const *argv[]) {
-  #if TEST == 1
-  return RSGtest(argc,argv);
-  #endif
-  #if TEST != 1
   ncursesGui program;
   program.init();
   program.mainLoop();
   program.exit();
-
   return 0;
-  #endif
-}
-
-void printList(list<string> vlist, string sep){
-  list<string> :: iterator itr;
-  for ( itr = vlist.begin(); itr != vlist.end(); itr++){
-    cout << (*itr) << sep;
-  }
-  cout << endl;
-}
-void writeList(list<string> vlist,string name){
-  ofstream file;
-  file.open(name);
-  list<string> :: iterator itr;
-  for ( itr = vlist.begin(); itr != vlist.end(); itr++){
-    file << (*itr) << '\n';
-  }
-  file.close();
 }
